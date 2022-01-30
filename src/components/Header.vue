edafce1d7239b7a9f231c55f1555afef8f5c1619
YahorDanchanka <danchenko.dev@yandex.by>
2022-01-30 14:19:04 +0300

    Создан компонент Header

---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <router-link :to="{ name: 'home' }" class="navbar-brand">SPA Blog</router-link>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li v-for="link in links" class="nav-item">
+            <router-link :to="link.to" class="nav-link" active-class="active">{{ link.label }}</router-link>
+          </li>
+        </ul>
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Введите запрос..." />
+          <button class="btn btn-outline-success" type="submit">Искать</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts" setup>
+import { computed, PropType } from 'vue'
+import { useStore } from 'vuex'
+import { ILink } from '@/components/header'
+
+const store = useStore()
+
+const props = defineProps({
+  links: {
+    type: Array as PropType<ILink[]>,
+    required: true,
+  },
+})
+</script>
--- src/views/layouts/Main.vue
+++ src/views/layouts/Main.vue
@@ -1,3 +1,12 @@
 <template>
+  <Header :links="links" />
   <router-view />
 </template>
+
+<script lang="ts" setup>
+import { computed } from 'vue'
+import { ILink } from '@/components/header'
+import Header from '@/components/Header.vue'
+
+const links = computed<ILink[]>(() => [{ label: 'Главная', to: { name: 'home' } }])
+</script>
